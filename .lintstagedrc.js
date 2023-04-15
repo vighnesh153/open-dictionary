7974e9d123138ccd60662401b32782c3e36d93de
vighnesh153 <vighnesh.raut13@gmail.com>
2023-04-15 21:27:03 +0530

    build: add js to lint-staged

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,25 +1,25 @@
-import {ESLint} from 'eslint';
+import { ESLint } from 'eslint';
 
 const removeIgnoredEslintFiles = async (files) => {
-    const eslint = new ESLint();
-    const ignoredFiles = await Promise.all(files.map((file) => eslint.isPathIgnored(file)));
-    const filteredFiles = files.filter((_, i) => !ignoredFiles[i]);
-    return filteredFiles.join(' ');
+  const eslint = new ESLint();
+  const ignoredFiles = await Promise.all(files.map((file) => eslint.isPathIgnored(file)));
+  const filteredFiles = files.filter((_, i) => !ignoredFiles[i]);
+  return filteredFiles.join(' ');
 };
 
 export default {
-    '*.{mdx,ts}': ['eslint --max-warnings=0 --fix', 'prettier --write'],
-    '*.json': ['prettier --write'],
-    // '*.{ts,tsx}': async (files) => {
-    //   const eslintFiles = await removeIgnoredEslintFiles(files);
-    //   return [
-    //     // ...eslintFiles.map((file) => `eslint --max-warnings=0 --fix ${file}`),
-    //     `eslint --max-warnings=0 --fix ${eslintFiles}`,
-    //     // Doing the following "map" approach to avoid this error
-    //     // "ENAMETOOLONG: name too long"
-    //     ...files.map((file) => `prettier --write ${file}`),
-    //   ];
-    // },
-    // '*.{js,jsx}': ['prettier --write'],
-    // '*.md': ['prettier --write'],
+  '*.{mdx,ts}': ['eslint --max-warnings=0 --fix', 'prettier --write'],
+  '*.{json,js}': ['prettier --write'],
+  // '*.{ts,tsx}': async (files) => {
+  //   const eslintFiles = await removeIgnoredEslintFiles(files);
+  //   return [
+  //     // ...eslintFiles.map((file) => `eslint --max-warnings=0 --fix ${file}`),
+  //     `eslint --max-warnings=0 --fix ${eslintFiles}`,
+  //     // Doing the following "map" approach to avoid this error
+  //     // "ENAMETOOLONG: name too long"
+  //     ...files.map((file) => `prettier --write ${file}`),
+  //   ];
+  // },
+  // '*.{js,jsx}': ['prettier --write'],
+  // '*.md': ['prettier --write'],
 };
