872710edeb05f64e79024862b16463b69abdd39e
vighnesh153 <vighnesh.raut13@gmail.com>
2023-04-16 11:56:26 +0530

    ci: use jitterbit/get-changed-files github action

---
+++ src/pull-request-test.ts
@@ -0,0 +1,6 @@
+/* eslint-disable no-console */
+console.log('***************************************');
+console.log('logging info...');
+console.log('process.argv', process.argv);
+console.log('cwd', process.cwd());
+console.log('__dirname', __dirname);
--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,8 @@ import { defineConfig } from 'tsup';
 export default defineConfig(() => ({
   entry: {
     'new-word': './src/new-word.ts',
-    'playground': './src/playground.ts',
+    playground: './src/playground.ts',
+    pullRequestTest: './src/pull-request-test.ts',
   },
   splitting: false,
   clean: true,
